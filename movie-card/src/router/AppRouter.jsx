b9bd8826d1e3726de87af903b10bc3141f34f8c9
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-28 23:15:33 +0000

    helpers are added

--- movie-card/src/router/AppRouter.jsx
+++ movie-card/src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Main from "../pages/Main";
+import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
+import Main from "../pages/Main";
 import MovieDetail from "../pages/MovieDetail";
 import Register from "../pages/Register";
 
 const AppRouter = () => {
     return <BrowserRouter>
+    <Navbar />
     <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
