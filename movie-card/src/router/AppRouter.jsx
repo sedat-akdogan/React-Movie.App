239b8c8f6fe15ffc2d0fe93649560503d895925d
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-30 01:18:56 +0000

    App Router updated

--- movie-card/src/router/AppRouter.jsx
+++ movie-card/src/router/AppRouter.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import MovieDetail from "../pages/MovieDetail";
 import Register from "../pages/Register";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
-    return <BrowserRouter>
-    <Navbar />
-    <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/details/:id" element={<MovieDetail />} />
-    </Routes>
-    </BrowserRouter>;
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/details/:id" element={<PrivateRouter />}>
+                    <Route path="" element={<MovieDetail />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default AppRouter;
